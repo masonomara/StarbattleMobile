5f885ff3425266a2da7a2e63e63cdb7a8b5fe130
masonomara <mason.omara@gmail.com>
2026-02-14 10:00:21 -0500

    Set up navigation

---
+++ src/navigation.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { HomeScreen } from './screens/HomeScreen';
+import { PackScreen } from './screens/PackScreen';
+import { PuzzleScreen } from './screens/PuzzleScreen';
+
+export type RootStackParams = {
+  Home: undefined;
+  Pack: { packId: string };
+  Puzzle: { packId: string; puzzleIndex: number };
+};
+
+const Stack = createNativeStackNavigator<RootStackParams>();
+
+export function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerBackButtonDisplayMode: 'minimal',
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Pack" component={PackScreen} />
+        <Stack.Screen name="Puzzle" component={PuzzleScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
