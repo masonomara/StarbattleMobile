2c87e5993df2440a79db30aa4710d285f6ab414a
masonomara <mason.omara@gmail.com>
2026-02-15 16:39:49 -0500

    Inlined user provider

--- App.tsx
+++ App.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Navigation } from './src/navigation';
-import { UserProvider } from './src/components/UserProvider';
+import { useUserStore } from './src/stores/userStore';
 
 export default function App() {
+  useEffect(() => {
+    useUserStore.getState().initialize();
+  }, []);
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider>
-        <UserProvider>
-          <Navigation />
-        </UserProvider>
+        <Navigation />
       </SafeAreaProvider>
     </GestureHandlerRootView>
   );
